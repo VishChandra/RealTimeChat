bbca474de343cb50c2884aef09e34d67715fc558
Vishal Chandra <vchandra@arrow.ca>
2018-08-30 11:34:43 -0700

    Added files

--- RealTimeChat/Startup.cs
+++ RealTimeChat/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WebSocketManager;
 
 namespace RealTimeChat
 {
@@ -21,11 +23,12 @@ namespace RealTimeChat
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddWebSocketManager();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -36,6 +39,9 @@ namespace RealTimeChat
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            app.UseWebSockets();
+            app.MapWebSocketManager("/chat", serviceProvider.GetService<ChatHandler>());
 
             app.UseStaticFiles();
 
--- RealTimeChat/wwwroot/js/site.js
+++ RealTimeChat/wwwroot/js/site.js
@@ -1,1 +1,19 @@
 ﻿// Write your JavaScript code.
+
+$(document).ready(function ()
+{
+    var connection = new WebSocketManager.Connection("ws://localhost:5051/chat");
+    conection.enableLogging = true;
+
+    conenction.connectionMethods.onConnected = () =>
+    {
+
+    }
+
+    connection.connectionMethods.onDisconnected = () =>
+    {
+
+    }
+
+    connection.Start();
+});
